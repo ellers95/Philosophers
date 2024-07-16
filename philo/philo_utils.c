2f4863c640cd70698ce91c9c3a543e85e66c6801
Ella Taattola <etaattol@c1r1p10.hive.fi>
2024-07-16 15:38:32 +0300

    added debugging statements to god function cause it loops endlessly

--- philo/philo_utils.c
+++ philo/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: etaattol <etaattol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 13:34:32 by etaattol          #+#    #+#             */
-/*   Updated: 2024/07/12 18:03:22 by etaattol         ###   ########.fr       */
+/*   Updated: 2024/07/16 15:36:08 by etaattol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,14 @@ void    ft_usleep(size_t time_to_sleep)
 int check_death(t_philo *philo)
 {
     size_t  current_time;
+    size_t  time_since_last_meal;
     size_t  time_to_die;
 
     current_time = get_time_ms();
-    time_to_die = (size_t)philo->attributes->time_to_die;
-    
-    if ((current_time - philo->last_meal) > time_to_die)
+    time_to_die = (size_t)(philo->attributes->time_to_die * 1000);
+    time_since_last_meal = current_time - philo->last_meal;
+
+    if (time_since_last_meal > time_to_die)
     {
         pthread_mutex_lock(philo->death);
         philo->is_dead = 1;
@@ -54,16 +56,31 @@ void    god(t_philo *philos, t_attributes *attributes)
     
     i = 0;
     everyone_ate = 0;
+    printf("Entering outer loop\n");
     while (!everyone_ate)
     {
+        printf("=== Beginning of outer loop iteration ===\n");  // Debug
         everyone_ate = 1;
+        i = 0;
         while (i < attributes->number_of_philos)
         {
+            printf("Checking philosopher %d\n", i);  // Debug
             if (check_death(&philos[i]))
+            {
+                printf("Philosopher %d is dead\n", i);  // Debug
                 return ;
-            if (attributes->number_of_meals != -1 && philos[i]. times_eaten < attributes->number_of_meals)
+            }
+            printf("Times eaten by philosopher %d: %d\n", i, philos[i].times_eaten);  // Debug
+            printf("Number of meals allowed: %d\n", attributes->number_of_meals);  // Debug
+            if (attributes->number_of_meals == -1 || philos[i]. times_eaten < attributes->number_of_meals)
+            {
+                printf("Philosopher %d needs to eat more\n", i);  // Debug
                 everyone_ate = 0;
+            }
+            i++;
         }
+        i = 0;
+        printf("=== End of outer loop iteration ===\n");  // Debug
         usleep(1000);
     }
 }
